fa96f72623fef7a0c622dae7c059785f1038ad3b
eugenetangkj <dragoneugene10@gmail.com>
2024-09-16 22:49:14 +0800

    Add structure

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-blue-200 w-screen h-screen flex justify-center items-center">
+      <h1 className='text-2xl'>Good Goods</h1>
+    </div>
+  );
+}
---
+++ src/app/catalogue/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Catalogue() {
+    return (
+      <div className="bg-blue-200 w-screen h-screen flex justify-center items-center">
+        <h1 className='text-2xl'>Good Goods Catalogue</h1>
+      </div>
+    );
+  }
+  
