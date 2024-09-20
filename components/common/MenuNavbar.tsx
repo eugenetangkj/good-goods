53a27e7233c0ed29fc7788697996d4f38ce2be37
eugenetangkj <dragoneugene10@gmail.com>
2024-09-20 17:18:37 +0800

    Start on navbar responsiveness

--- components/common/MenuNavbar.tsx
+++ components/common/MenuNavbar.tsx
@@ -6,13 +6,14 @@ export function MenuNavbar() {
   return (
     <Navbar fluid rounded className="sticky top-0 z-50 bg-good-goods-blue-100">
       <NavbarBrand as={Link} href="/">
-        <span className="self-center whitespace-nowrap text-2xl font-semibold text-good-goods-blue-900 ">🩵 Welcome to Good Goods</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold text-good-goods-blue-900">🩵 Welcome to Good Goods</span>
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
-        <NavbarLink href="#" className='text-lg text-good-goods-blue-900 duration-200 mx-4'>About</NavbarLink>
-        <NavbarLink href="#" className='text-lg text-good-goods-blue-900 duration-200 mx-4'>Businesses</NavbarLink>
-        <NavbarLink href="#" className='text-lg text-good-goods-blue-900 duration-200 mx-4'>Recommend</NavbarLink>
+        <NavbarLink href="#" className='text-base text-good-goods-blue-900 duration-200 mx-2 lg:mx-4'>About</NavbarLink>
+        <NavbarLink href="#" className='text-base text-good-goods-blue-900 duration-200 mx-2 lg:mx-4'>Businesses</NavbarLink>
+        <NavbarLink href="#" className='text-base text-good-goods-blue-900 duration-200 mx-2 lg:mx-4'>Recommend</NavbarLink>
+
       </NavbarCollapse>
     </Navbar>
   );
