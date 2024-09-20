929828f6d33620a0a31e3a88c43fd9a4f2294f2a
eugenetangkj <dragoneugene10@gmail.com>
2024-09-21 01:05:05 +0800

    Start on recommendation form

--- src/app/recommend/page.tsx
+++ src/app/recommend/page.tsx
@@ -10,7 +10,7 @@ export default function Recommend() {
       {/* Body */}
       <div className='flex flex-col justify-center p-4 space-y-16 mt-15vh'>
         <div className='flex flex-col justify-center space-y-8'>
-          <h2 className='text-good-goods-blue-900 font-semibold text-2xl sm:text-3xl lg:text-4xl'>Know of a socially conscious business not found in Good Goods?<br/>Let us know!</h2>
+          <h2 className='text-good-goods-blue-900 font-semibold text-2xl sm:text-3xl lg:text-4xl sm:w-3/4'>Know of a socially conscious business not found in Good Goods? Let us know!</h2>
           <RecommendForm />
           
         </div>
--- src/components/recommend/RecommendForm.tsx
+++ src/components/recommend/RecommendForm.tsx
@@ -6,29 +6,142 @@ import { useState } from "react";
 //Form seen in recommendation page
 export function RecommendForm() {
 
-    //User's input value
-    const [userInput, setUserInput] = useState('');
+    //Email input value
+    const [emailInput, setEmailInput] = useState('');
+    //Business name input value
+    const [businessNameInput, setBusinessNameInput] = useState('');
+    //Business contact input value
+    const [businessContactInput, setBusinessContactInput] = useState('');
 
-   
-    //Function that runs when user submits recommendation form
-    const handleSubmitForm = (event : React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();    
-        console.log("Submitted form:", userInput);
+
+
+
+
+    //Function that runs when user changes email input
+    const handleEmailInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmailInput(event.target.value);
     };
+    //Function that runs when user changes email input
+     const handleBusinessNameInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setBusinessNameInput(event.target.value);
+    };
+    //Function that runs when user changes enterprise contact
+    const handleBusinessContactInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setBusinessContactInput(event.target.value);
+    };
+
+
+
+
+
+    //Function that runs when user submits recommendation form
+    const handleSubmitQuery = async (event : React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+          let newMessage = "From: " + emailInput + "\n\n" + businessNameInput + businessContactInput;
+          await sendEmail("subject", newMessage);
+        //   openModal("Message successfully sent! We will get in touch soon.");
+
+        //Reset fields
+        setEmailInput("");
+        setBusinessNameInput("");
+        setBusinessContactInput("");
+        } catch (error) {
+          console.error("Error sending email:", error);
+        //   openModal("We could not receive your message. Please try again.");
+        }
+      };
+
+
+
+
+      const sendEmail = async (subject: string, message: string) => {
+        try {
+            const response = await fetch("/api/sendContactEmail", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ subject, message }),
+            });
+        
+            if (!response.ok) {
+            throw new Error(
+                `Email API request failed with status: ${response.status}`
+            );
+            }
+        
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+            throw new Error(`Error sending email: ${(error as Error).message}`);
+        }
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return (
         <div className='flex flex-col space-y-8'>
-            <form className="max-w-sm">
+            <form className="flex flex-col w-3/4 2xl:w-1/2" onSubmit={ handleSubmitQuery }>
                 {/* Email field */}
                 <div className="mb-5">
-                    <label htmlFor="email" className="block mb-2 font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input type="email" id="email" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 px-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    <label htmlFor="email" className="block mb-2 font-medium text-gray-900">Your Email</label>
+                    <input type="email"
+                        id="email"
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 px-3 "
+                        value={ emailInput }
+                        onChange={ handleEmailInputChange }
+                        required
+                    />
                 </div>
-                
+
+                {/* Name of business */}
+                <div className="mb-5">
+                    <label htmlFor="nameOfBusiness" className="block mb-2 font-medium text-gray-900">Name of Business</label>
+                    <input type="text"
+                        id="nameOfBusiness"
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 px-3 "
+                        value={ businessNameInput }
+                        onChange={ handleBusinessNameInputChange }
+                        required
+                    />
+                </div>
 
 
-                
-                <button type="submit" className="text-white bg-good-goods-blue-900 hover:bg-sky-700 rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
+                {/* Business Contact */}
+                <div className="mb-5">
+                    <label htmlFor="businessContact" className="block mb-2 font-medium text-gray-900">Website/Location of Business</label>
+                    <input type="text"
+                        id="businessContact"
+                        className="bg-white border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 px-3 "
+                        value={ businessContactInput }
+                        onChange={ handleBusinessContactInputChange }
+                        required
+                    />
+                </div>
+
+                {/* Submit button */}
+                <button type="submit" className="text-white bg-good-goods-blue-900 hover:bg-sky-700 rounded-full text-sm w-full sm:w-auto px-5 sm:px-16 py-2.5 text-center self-end duration-200">Submit</button>
             </form>
 
 
