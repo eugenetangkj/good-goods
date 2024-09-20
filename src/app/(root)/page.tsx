19ee276d60562f2fcac63266ed8f17ce3d9045c2
eugenetangkj <dragoneugene10@gmail.com>
2024-09-20 19:47:44 +0800

    Add footer

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,16 @@
 import { AISearchBar } from "@/components/AISearchBar";
 import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-good-goods-blue-100 w-screen h-screen p-8">
+    <div className='bg-good-goods-blue-100 w-screen h-screen p-8'>
       <Navbar />
-      <div className='flex flex-col justify-center h-screen-4/5 p-4 space-y-8 mt-10vh'>
-        <h2 className='text-good-goods-blue-900 font-semibold text-2xl sm:text-3xl lg:text-4xl'>What kind of good are you looking to do today?</h2>
-        <AISearchBar />
-      </div> 
+        <div className='flex flex-col justify-center h-screen-4/5 p-4 space-y-8 mt-10vh'>
+          <h2 className='text-good-goods-blue-900 font-semibold text-2xl sm:text-3xl lg:text-4xl'>What kind of good are you looking to do today?</h2>
+          <AISearchBar />
+        </div> 
+      <Footer />
     </div>
   );
 }
