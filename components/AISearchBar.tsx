f4c19a75e210c240b8401cfede9ec81cdff85853
eugenetangkj <dragoneugene10@gmail.com>
2024-09-20 16:16:50 +0800

    Adjust styles

--- components/AISearchBar.tsx
+++ components/AISearchBar.tsx
@@ -42,6 +42,7 @@ export function AISearchBar() {
         <form className="flex flex-col gap-4 space-y-8" onSubmit={ handleSubmitQuery }>
             <div>
                 <div className="relative w-4/5 mx-auto">
+                    {/* Input field */}
                     <input
                         className="block border border-gray-300 bg-white text-gray-900 text-lg focus:border-cyan-500 focus:ring-cyan-500 p-4 pr-12 rounded-full"
                         style={{ width: "75%"}}
@@ -52,10 +53,12 @@ export function AISearchBar() {
                         onChange={handleInputChange}
                         required
                     />
+
+                    {/* Submit button */}
                     <button
                         type="submit"
-                        className="absolute top-1/2 transform -translate-y-1/2 text-good-goods-blue-900 text-2xl"
-                        style={{ left: "70%"}}>
+                        className="absolute top-1/2 transform -translate-y-1/2 text-good-goods-blue-900 hover:text-gray-900 duration-200"
+                        style={{ left: "70%", fontSize: "1.875rem"}}>
                         <FaArrowCircleUp />
                     </button>
                 </div>
