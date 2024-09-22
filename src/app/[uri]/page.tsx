338c6c346e824076a347fe8f782eeb3a8be61fa6
kohkaixun <kkx1929@gmail.com>
2024-09-23 00:29:10 +0800

    Render goods offered

--- src/app/[uri]/page.tsx
+++ src/app/[uri]/page.tsx
@@ -22,6 +22,7 @@ interface Enterprise {
     "Region": string[];
     "Type of goods offered": string[];
     "Opening hours": string[];
+    "Website": string;
 }
 
 function getEnterpriseByParam(param: string) {
@@ -34,6 +35,7 @@ function EnterprisePage() {
     const [loading, setLoading] = useState(true);
     const [enterprise, setEnterprise] = useState<Enterprise | null>(null);
     const [imageError, setImageError] = useState(false);
+    const [truncatedGoods, setTruncatedGoods] = useState<string[]>([]);
 
     useEffect(() => {
         if (enterpriseName) {
@@ -41,6 +43,7 @@ function EnterprisePage() {
                 ? getEnterpriseByParam(enterpriseName[0])
                 : getEnterpriseByParam(enterpriseName);
             setEnterprise(foundEnterprise || null);
+            setTruncatedGoods(foundEnterprise === undefined ? [] : foundEnterprise["Type of goods offered"].slice(0, 10));
             setLoading(false);
         }
     }, [enterpriseName]);
@@ -52,8 +55,9 @@ function EnterprisePage() {
             {!loading && enterprise && 
             <div className='flex flex-col justify-center p-4 space-y-16 mt-15vh'>
                 <div className='flex flex-col justify-center space-y-8'>
+                    {/* Enterprise Image */}
                     {enterprise["Enterprise picture relative path"] && !imageError && 
-                    <div className='relative w-full h-96 overflow-hidden rounded-lg'>
+                    <a href={enterprise['Website']} className='relative w-full h-96 overflow-hidden rounded-lg'>
                     <Image 
                             src={enterprise["Enterprise picture relative path"]}
                             alt={enterprise["Enterprise Name"]}
@@ -64,17 +68,62 @@ function EnterprisePage() {
                                 console.log('Enterprise pictuure reelative path invalid.');
                                 setImageError(true);
                             }}
-                    /></div>}
-                    {/* <div className='h-80'> */}
-                    <h2 className='text-good-goods-blue-900 font-semibold text-2xl sm:text-3xl lg:text-4xl'>{enterprise['Enterprise Name']}</h2>
-                    {/* </ div> */}
+                    />
+                    </a>}
+
+                    {/* Enterprise Name */}
+                    <div>
+                        <a 
+                            href={enterprise['Website']} 
+                            className='text-good-goods-blue-900 font-semibold text-4xl sm:text-5xl lg:text-6xl'
+                            style={{ 
+                                display: 'inline-block', 
+                                whiteSpace: 'nowrap',  
+                                overflow: 'hidden', 
+                                textOverflow: 'ellipsis', 
+                                maxWidth: '100%', 
+                            }}
+                        >
+                            {enterprise['Enterprise Name']}
+                        </a>
+                    </div>
+
+                    {/* Enterprise Goods */}
+                    <div className="flex flex-wrap max-w-[75%]">
+                        {truncatedGoods.map((good, index) => (
+                            <span 
+                                key={index} 
+                                className="bg-blue-100 text-blue-800 text-xs sm:text-sm lg:text-lg font-medium px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300 mr-2 mb-2 overflow-hidden whitespace-nowrap text-ellipsis"
+                                style={{ maxWidth: '300px' }}
+                                title={good}
+                            >
+                                {good}
+                            </span>
+                        ))}
+                    </div>
                     
+                    {/* Render Locations with Opening Hours */}
+                    <div className='space-y-1'>
+                        {enterprise["Location"].map((location, index) => (
+                            <EnterpriseLocation 
+                                key={index} 
+                                address={location} 
+                                openingHours={enterprise["Opening hours"][index]} 
+                            />
+                        ))}
+                    </div>
+
                     {/* Enterprise Impact */}
-                    <h3 className='text-good-goods-blue-900 text-l sm:text-2xl lg:text-3xl' style={{ textAlign: 'justify' }}>
+                    <p className='text-good-goods-blue-900 text-l sm:text-2xl lg:text-3xl' style={{ textAlign: 'justify' }}>
                         {enterprise['Detailed impact'].split('\n').map((line, index) => (
                             <span key={index}>{line}<br /><br /></span>
                         ))}
-                    </h3>
+                    </p>
+                    {/* Visit Line */}
+                    <p className='text-good-goods-blue-900'>
+                        Visit <a href={enterprise['Website']} className='text-blue-500 underline'>{enterprise['Enterprise Name']}</a> now!
+                    </p>
+
                     {/* Render Locations with Opening Hours */}
                     <div className='space-y-1'>
                         {enterprise["Location"].map((location, index) => (
