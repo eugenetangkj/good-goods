4f8626d18c73d8b7850791e53bc656f49551e748
eugenetangkj <dragoneugene10@gmail.com>
2024-09-24 00:57:31 +0800

    Add loading indicator

--- src/components/home/AISearchBar.tsx
+++ src/components/home/AISearchBar.tsx
@@ -17,6 +17,7 @@ export function AISearchBar() {
     const [format, setFormat] = useState('');
     const [region, setRegion] = useState('');
     const [goodsType, setGoodsType] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
@@ -27,6 +28,7 @@ export function AISearchBar() {
 
     const handleSubmitQuery = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        setIsLoading(true);
         console.log("Submitted query:", userInput);
         try {
             const response = await fetch("/api/queryChatbot", {
@@ -40,9 +42,11 @@ export function AISearchBar() {
             const newDisplay = docs.filter(x => keep.includes(x.ID));
             console.log(newDisplay)
             setDisplay(newDisplay);
+            setIsLoading(false);
         } catch {
             console.log('Ran into an error.');
             setUserInput("No result found. Please try a different search parameter.")
+            setIsLoading(false);
         }
     };
 
@@ -64,9 +68,13 @@ export function AISearchBar() {
         console.log(filteredDocs)
        
     }
+
+
     return (
         <div className='flex flex-col space-y-8'>
             <form className="flex flex-col gap-4 space-y-8" onSubmit={handleSubmitQuery}>
+
+                {/* Search bar */}
                 <div className="relative lg:w-4/5">
                     <input
                         className="block border border-gray-300 bg-white text-gray-900 text-sm sm:text-base md:text-lg focus:border-cyan-500 focus:ring-cyan-500 p-3 lg:p-4 pr-12 rounded-full w-full sm:w-4/5 lg:w-3/4"
@@ -77,13 +85,19 @@ export function AISearchBar() {
                         onChange={handleInputChange}
                         required
                     />
-                    <button
-                        type="submit"
-                        className="absolute top-1/2 left-0.9 sm:left-0.7 text-xl sm:text-2xl lg:text-3xl transform -translate-y-1/2 text-good-goods-blue-900 hover:text-gray-900 duration-200"
-                    >
-                        <FaArrowCircleUp />
-                    </button>
+                    {
+                    (isLoading)
+                    ?  <div className='absolute top-1/2 left-0.9 sm:left-0.7 transform -translate-y-1/2'><svg aria-hidden="true" className="w-4 h-4 sm:w-6 sm:h-6 lg:w-8 lg:h-8  text-gray-400 animate-spin fill-good-goods-blue-900" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg></div>
+                    :   <button
+                    type="submit"
+                    className="absolute top-1/2 left-0.9 sm:left-0.7 text-xl sm:text-2xl lg:text-3xl transform -translate-y-1/2 text-good-goods-blue-900 hover:text-gray-900 duration-200"
+                >
+                    <FaArrowCircleUp />
+                </button>      
+                    
+                    }
                 </div>
+
                 <div className={`mt-4 text-sm sm:text-base block ${userInput.length === maxNumberOfCharacters ? 'text-red-500' : 'text-gray-700'}`}>
                     {userInput.length} / {maxNumberOfCharacters} characters
                 </div>
